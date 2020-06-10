9fe824bd8f9f4d8a557aa2eb90fb7cfa111673bf
lalinsch <laloschjetnan@gmail.com>
2020-06-10 18:44:17 +0100

    Updated with comments

--- part04-Part04_31.SportStatistics/src/main/java/Game.java
+++ part04-Part04_31.SportStatistics/src/main/java/Game.java
@@ -10,11 +10,13 @@
  */
 public class Game {
 
+    //game attributes
     private String homeTeam;
     private String awayTeam;
     private int homePoints;
     private int awayPoints;
 
+    //game constructor (useful for adding that to the arraylist later on)
     public Game(String homeTeam, String awayTeam, int homePoints, int awayPoints) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
@@ -22,6 +24,7 @@ public class Game {
         this.awayPoints = awayPoints;
     }
 
+    //getters
     public String getHomeTeam() {
         return this.homeTeam;
     }
@@ -30,6 +33,7 @@ public class Game {
         return this.awayTeam;
     }
 
+    // figures out the winner for a game
     public String getWinner() {
         if (this.homePoints > this.awayPoints) {
             return this.homeTeam;
@@ -38,6 +42,7 @@ public class Game {
         }
     }
 
+    //figures out the loser for a game
     public String getLoser() {
         if (this.homePoints > this.awayPoints) {
             return this.awayTeam;
@@ -46,6 +51,7 @@ public class Game {
         }
     }
 
+    //not used in this exercise but useful to display the data more clearly to the user 
     @Override
     public String toString() {
         return "Score: H: " + this.homeTeam + " " + this.homePoints + " vs " + this.awayPoints + " A:" + this.awayTeam + "\n";
--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -8,13 +8,16 @@ public class SportStatistics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        //asks user for filename 
         System.out.println("File:");
         String file = scan.nextLine();
         ArrayList<Game> games = new ArrayList();
         int gameCount = 0;
         int winCount = 0;
         int lossCount = 0;
-
+        //initialises ints for game, wins and losses counts
+        
+        //reads the file and adds it to an ArrayList of the type Game (comma separated)
         try ( Scanner fileReader = new Scanner(Paths.get(file))) {
 
             while (fileReader.hasNextLine()) {
@@ -27,27 +30,32 @@ public class SportStatistics {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-
+        
+        //asks user what team they want to find data from
         System.out.println("Team:");
         String team = scan.nextLine();
 
         //finds how many games each team has played by looking at the input
         for (Game g : games) {
+            //performs the object methods to calculate winners and losers
             g.getWinner();
             g.getLoser();
             if (g.getHomeTeam().equals(team) || g.getAwayTeam().equals(team)) {
                 gameCount++;
             }
-
+            
+            //counts how many times the team has been winner
             if (g.getWinner().equals(team)) {
                 winCount++;
             }
-
+            //counts how many teams the team has been a loser
             if (g.getLoser().equals(team)) {
                 lossCount++;
             }
 
         }
+        
+        //prints results to user
         System.out.println("Games: " + gameCount);
         System.out.println("Wins: " + winCount);
         System.out.println("Losses: " + lossCount);
